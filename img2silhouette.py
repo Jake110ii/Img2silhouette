ecedc3304b32227c7f43d9c2aeb28682e13d76fd
hayata-morita <hayata.morita@tri.global>
2024-02-05 17:54:49 -0500

    first

--- img2silhouette.py
+++ img2silhouette.py
@@ -3,10 +3,13 @@ import numpy as np
 import matplotlib.widgets as wg
 import matplotlib.gridspec as gridspec
 import matplotlib
+from PIL import Image
+import matplotlib.image as mpimg
+import os
 matplotlib.use('WebAgg')
 
-import utils.NearestNeighbor as near
-
+import utils.PointManipulator as pmani
+import utils.file_selecter as fsel
 
 class Img2silhouette():
     def __init__(self, gs):
@@ -14,6 +17,7 @@ class Img2silhouette():
         self.ax1 = plt.subplot(gs[1, 0])
         self.ax2 = plt.subplot(gs[1, 1])
         self.ax3 = plt.subplot(gs[1, 2])
+        self.ax4 = plt.subplot(gs[1, 3])
         self.ln, = self.ax0.plot([],[],'bo')
         self.cur, = self.ax0.plot([],[],'ro')
         self.x = []
@@ -36,25 +40,32 @@ class Img2silhouette():
         self.mo_id = None
         self.onc_id = None
         self.onp_id = None
-   
-    def onclick(self, event):
-        if not (0 < event.xdata <=1 and 0 < event.ydata <= 1):
-            x = event.xdata
-            y = event.ydata
-            self.x.append(x)
-            self.y.append(y)
+        
+        # image file
+        self.car_file_path = "./input/car.png"
+        self.imshow_id = None
 
-            self.cur.set_data(x, y)
-            self.ln.set_data(self.x, self.y)
-            plt.draw()
+    def onclick(self, event):
+        if event.xdata != None and event.ydata != None:
+            if not (0 < event.xdata <=1 and 0 < event.ydata <= 1):
+                x = event.xdata
+                y = event.ydata
+                self.x.append(x)
+                self.y.append(y)
+
+                self.cur.set_data(x, y)
+                self.ln.set_data(self.x, self.y)
+                plt.draw()
 
     def btn_silhouette_click(self, event):
         if self.line_pre is not None:
             self.line_pre.remove()
 
         # nearest sort
-        self.x, self.y = near.nearest_sort(self.x, self.y)
-        self.line_pre, = self.ax0.plot(self.x + [self.x[0]], self.y + [self.y[0]], "o-", picker=15)
+        #self.x, self.y = near.nearest_sort(self.x, self.y)
+        if self.first_flg != "first":
+            self.x, self.y = pmani.sort_shortest_distance(self.x[:-1], self.y[:-1], self.x[-1], self.y[-1])
+        self.line_pre, = self.ax0.plot(self.x + [self.x[0]], self.y + [self.y[0]], "o-", picker=15, color="black")
         self.cur.set_data(self.x[0], self.y[0])
         self.ln.set_data(self.x, self.y)
         plt.draw()
@@ -119,12 +130,29 @@ class Img2silhouette():
             self.ax0.set_title('mode = {}'.format(self.mode_switch))
             plt.draw()
 
+    def img_input(self):
+        """
+        input car image and set its background
+        """
+        img = Image.open(self.car_file_path)
+        #fig.patch.set_facecolor('white')
+        self.ax0.set_facecolor('white')
+
+        # 背景に画像を貼り付け
+        self.imshow_id = self.ax0.imshow(img, extent=[0, img.size[0], 0, img.size[1]], aspect='equal', alpha=0.3)
+
+        # その他のプロット設定など...
+        self.ax0.set_xlim(0-img.size[0]*0.1, img.size[0]*(1+0.1))
+        self.ax0.set_ylim(0-img.size[1]*0.1, img.size[1]*(1+0.1))
+
+
 class Btn2mode:
     def __init__(self, img2silhouette):
         self.img2silhouette = img2silhouette
         self.btn_silh = wg.Button(img2silhouette.ax1, 'silhouette', color='#ffffff', hovercolor='#38b48b')
         self.btn_clear = wg.Button(img2silhouette.ax2, 'clear', color='#ffffff', hovercolor='#38b48b')
         self.btn_switch_mode = wg.Button(img2silhouette.ax3, 'switch mode', color='#ffffff', hovercolor='#38b48b')
+        self.btn_file_upload = wg.Button(img2silhouette.ax4, 'file upload', color='#ffffff', hovercolor='#38b48b')
         
     def btn_silhouette_click(self, event):
         self.img2silhouette.btn_silhouette_click(event)
@@ -146,23 +174,39 @@ class Btn2mode:
             print("Switched to motion mode")
             self.img2silhouette.setup_callbacks()
             self.btn_flg = "on"
+            self.img2silhouette.line_pre.set_color('red')
         elif self.img2silhouette.mode_switch == "motion":
             self.img2silhouette.mode_switch = "onclick"
             print("Switched to onclick mode")
             self.img2silhouette.setup_callbacks()
             self.img2silhouette.btn_flg = "on"
+            self.img2silhouette.line_pre.set_color('black')
 
         self.img2silhouette.ax0.set_title('mode = {}'.format(self.img2silhouette.mode_switch))
         plt.draw()
+
+    def btn_file_upload_click(self, event):
+        self.img2silhouette.car_file_path = fsel.open_file_dialog()
+        if os.path.exists(self.img2silhouette.car_file_path):
+            self.img2silhouette.imshow_id.remove()
+            self.img2silhouette.img_input()
+            plt.draw()
 
     def setup_callbacks(self):
         self.btn_silh.on_clicked(self.btn_silhouette_click)
         self.btn_clear.on_clicked(self.btn_clear_click)
         self.btn_switch_mode.on_clicked(self.btn_switch_mode_click)
+        self.btn_file_upload.on_clicked(self.btn_file_upload_click)
 
 
 fig = plt.figure(figsize=(8, 6))
-gs = gridspec.GridSpec(2, 3, height_ratios=(8, 1))
-img2silhouette = Img2silhouette(gs)
-img2silhouette.setup_callbacks()
-plt.show()
+
+def main():
+    gs = gridspec.GridSpec(2, 4, height_ratios=(8, 1))
+    img2silhouette = Img2silhouette(gs)
+    img2silhouette.img_input()
+    img2silhouette.setup_callbacks()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
